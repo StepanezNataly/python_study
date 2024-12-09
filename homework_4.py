a56d0238d2f44f862ecc7d9d29bf8a138967b976
StepanezNataly <46973381+StepanezNataly@users.noreply.github.com>
2024-12-09 21:37:54 +0300

    Add files via upload

---
+++ homework_4.py
@@ -0,0 +1,5 @@
+count_of_completed_tasks = 12
+count_of_hours_used = 1.5
+name_course = 'Python'
+time_of_one_task = count_of_hours_used / count_of_completed_tasks
+print('Курс:',name_course,', всего задач:',count_of_completed_tasks,', затрачено часов:',count_of_hours_used,', среднее время выполнения',time_of_one_task,'часа.')
